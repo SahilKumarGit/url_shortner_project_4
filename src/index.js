d395ef8128de9b3556ed34eb1ac6cf232a32e713
SahilKumarGit <email.sahil@yahoo.com>
2022-05-17 12:28:43 +0530

    Base code!

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+
+mongoose
+  .connect(
+    "mongodb+srv://priya:priya1122@cluster0.zscq3.mongodb.net/group73Database", {
+      useNewUrlParser: true,
+    }
+  )
+  .then((result) => console.log("✅ MongoDb is connected"))
+  .catch((err) => console.log('⚠️ ',err.message));
+
+app.use("/", route);
+
+app.listen(port, () => {
+  console.log(`✅ Server is start on port ${port}`);
+});
---
+++ src/routes/route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+let router = express.Router();
+
+// router.get("/", (req, res) => {
+//     res.writeHead(301, { "Location": "http://google.com/" });
+//     res.end()
+// })
+
+module.exports = router;
